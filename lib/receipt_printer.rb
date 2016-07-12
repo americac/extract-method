ddaf3c1392fbd27a88e76c0a7d8a313875f50020
America Calleros <america.quintero@gmail.com>
2016-07-12 13:49:16 -0500

    Create several methods with single responsibility

--- lib/receipt_printer.rb
+++ lib/receipt_printer.rb
@@ -14,24 +14,45 @@ class ReceiptPrinter
 
   def print
     subtotal = subtotal_amount
-    output.puts divider
-    output.puts "subtotal: #{sprintf('$%.2f', subtotal)}"
-    output.puts "tax: #{sprintf('$%.2f', subtotal * TAX)}"
-    output.puts divider
-    output.puts "total: #{sprintf('$%.2f', subtotal + (subtotal * TAX))}"
+    print_divider
+    print_line(label:"subtotal", amount: format_amount(subtotal))
+    print_line(label:"tax", amount: format_amount(tax_amount(subtotal)))
+    print_divider
+    print_line(label:"total", amount: format_amount(total_amount(subtotal)))
   end
 
   private
 
   attr_reader :output, :items
 
+  def total_amount(amount)
+    amount + tax_amount(amount)
+
+  end
+
+  def tax_amount(amount)
+    amount * TAX
+  end
+
   def subtotal_amount
     items.reduce(0) do |sum, item|
       item_cost = COST[item]
-      output.puts "#{item}: #{sprintf('$%.2f', item_cost)}"
+      print_line(label: item, amount: format_amount(item_cost))
 
       sum + item_cost.to_i
     end
+  end
+
+  def format_amount(amount)
+    sprintf('$%.2f', amount)
+  end
+
+  def print_line(label:, amount:)
+    output.puts "#{label}: #{amount}"
+  end
+
+  def print_divider
+    output.puts divider
   end
 
   def divider

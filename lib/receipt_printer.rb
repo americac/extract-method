b73e1ac427e73e166098b6318be3ed4ee9eb0334
America Calleros <america.quintero@gmail.com>
2016-07-12 13:20:50 -0500

    Extract submount into a method

--- lib/receipt_printer.rb
+++ lib/receipt_printer.rb
@@ -13,13 +13,7 @@ class ReceiptPrinter
   end
 
   def print
-    subtotal = items.reduce(0) do |sum, item|
-      item_cost = COST[item]
-      output.puts "#{item}: #{sprintf('$%.2f', item_cost)}"
-
-      sum + item_cost.to_i
-    end
-
+    subtotal = subtotal_amount
     output.puts divider
     output.puts "subtotal: #{sprintf('$%.2f', subtotal)}"
     output.puts "tax: #{sprintf('$%.2f', subtotal * TAX)}"
@@ -30,6 +24,15 @@ class ReceiptPrinter
   private
 
   attr_reader :output, :items
+
+  def subtotal_amount
+    items.reduce(0) do |sum, item|
+      item_cost = COST[item]
+      output.puts "#{item}: #{sprintf('$%.2f', item_cost)}"
+
+      sum + item_cost.to_i
+    end
+  end
 
   def divider
     '-' * 13
